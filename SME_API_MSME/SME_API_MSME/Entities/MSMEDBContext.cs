8185383f9257675872d70a2c9f0b1de4664e2e48
osmepSI2025 <osmep.si2025@gmail.com>
2025-08-12 10:15:01 +0700

    call prod

--- SME_API_MSME/SME_API_MSME/Entities/MSMEDBContext.cs
+++ SME_API_MSME/SME_API_MSME/Entities/MSMEDBContext.cs
@@ -94,9 +94,9 @@ public partial class MSMEDBContext : DbContext
     public virtual DbSet<TSmeEconomicDevelopResult> TSmeEconomicDevelopResults { get; set; }
 
     public virtual DbSet<TSmeEconomicFactor> TSmeEconomicFactors { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=192.168.9.155;Database=bluecarg_SME_API_MSME;User Id=sa;Password=Osmep@2025;TrustServerCertificate=True;");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=192.168.9.155;Database=bluecarg_SME_API_MSME;User Id=sa;Password=Osmep@2025;TrustServerCertificate=True;");
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
